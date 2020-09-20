4d800365249d9234fc081aeaf459c77999360b77
rian <19516527+360ied@users.noreply.github.com>
2020-09-20 15:26:14 -0400

    do not care about http write errors

--- keepalive/keepalive.go
+++ keepalive/keepalive.go
@@ -3,17 +3,11 @@ package keepalive
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/ztrue/tracerr"
 )
 
 func KeepAlive() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		var _, err = fmt.Fprintf(w, "I'm alive")
-
-		if err != nil {
-			tracerr.PrintSourceColor(err)
-		}
+		_, _ = fmt.Fprintf(w, "I'm alive")
 	})
 
 	panic(http.ListenAndServe(":8080", nil))
